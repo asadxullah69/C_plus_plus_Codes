55c50a98db9bc0c95af0b6b437b1ae254a4ddcb8
Muhammad Asad Ullah <aaadullah37@gmail.com>
2025-05-27 23:24:31 +0500

    ITC Lab 7 task 4 done

---
+++ ITC Lab/task4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter a number:";
+    cin>>num;
+    cout<<"The number was: "<<num<<endl;
+    cout<<"The number in reverse digits is: ";
+    while (num>0)
+    {
+        cout<<num%10;
+        num = num/10;
+        if (num>0) cout<<",";
+    }
+    cout<<endl;
+    string num1;
+    cout<<"Enter the number again:";
+    cin>>num1;
+    cout<<"The number in digits is: ";
+    for(int i=0;i<num1.length();i++)
+    {
+        cout<<num1[i];
+        if(i<num1.length()-1) cout<<",";
+    }
+    
+    return 0;
+}   
