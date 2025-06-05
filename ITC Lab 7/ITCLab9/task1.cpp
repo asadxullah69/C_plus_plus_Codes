fa8b139b1cac8fcf829cd68562a5a69cb465254f
Muhammad Asad Ullah <aaadullah37@gmail.com>
2025-06-05 21:05:07 +0500

    lab 9 task 1,2,3 done

---
+++ ITC Lab 7/ITCLab9/task1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int rows;
+    cout << "Enter the number of rows for the hollow triangle: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++) 
+    {
+        for (int space = 1; space <= rows - i; space++) {
+            cout << " ";
+        }
+
+        // Print stars and spaces inside the triangle
+        for (int j = 1; j <= i; j++) {
+            if (j == 1 || j == i || i == rows)
+            {
+                cout << "* "; // Print border stars
+            } 
+            else 
+            {
+                cout << "  "; // Hollow space
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
