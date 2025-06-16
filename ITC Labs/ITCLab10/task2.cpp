28403f48fcd48cb56435fd2677c0823b76e09449
Muhammad Asad Ullah <aaadullah37@gmail.com>
2025-06-16 14:17:37 +0500

    Created lab 7 folder and task 2 of lab 10 done

--- ITC Labs/ITCLab10/task2.cpp
+++ ITC Labs/ITCLab10/task2.cpp
@@ -5,7 +5,8 @@ int main()
 {
     int list[7]={1,4,2,6,7,8,9};
     cout<<"Last roll number is: "<<list[6]<<endl;
-    list[6]=40;
+    list[4]=40;
+    list[2]=(list[0]+list[1])-2;
     for(int i=0;i<7;i++)
     {
         cout<<list[i];
